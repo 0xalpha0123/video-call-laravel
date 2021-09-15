757a1cc08bfbfef87ab0cca8d744c6ca1648f0a3
wayne-th <wayned.eth@gmail.com>
2021-09-15 16:07:35 +0300

    add tilio sdk and modify route

--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,7 @@ Auth::routes();
 
 Route::group(['prefix' => 'home'], function () {
     Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-});
-
-Route::group(['prefix' => 'room'], function () {
-    Route::get('', [App\Http\Controllers\VideoChatController::class, 'index'])->name('room');
-    Route::post('', [App\Http\Controllers\VideoChatController::class, 'generateRoom'])->name('room.generate');
-    Route::get('{reservation_id}', [App\Http\Controllers\VideoChatController::class, 'joinRoom'])->name('room.join');
-
-    // Route::get('join/{reservation_id}', [App\Http\Controllers\VideoChatController::class, 'join'])->name('videochat.join');
+    Route::post('/', [App\Http\Controllers\HomeController::class, 'generateRoom'])->name('home.generate');
 });
 
 // Route::group(['prefix' => 'videochat'], function () {
@@ -40,3 +33,5 @@ Route::group(['prefix' => 'room'], function () {
 Route::group(['prefix' => 'ajax'], function () {
     Route::post('videochat/authentication',  [App\Http\Controllers\VideoChatController::class, 'authentication'])->name('ajax.videochat.authentication');
 });
+
+Route::get('{reservation_id}', [App\Http\Controllers\VideoChatController::class, 'joinRoom'])->name('room.join');
